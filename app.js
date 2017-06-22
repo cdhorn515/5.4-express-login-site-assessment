faf4e85ef299f7ec84048cdf8b7877938ea6da7c
Christina Cornell <cdhorn515@gmail.com>
2017-06-22 15:57:41 -0400

    login working, need to style

---
+++ app.js
@@ -0,0 +1,98 @@
+var express = require('express');
+var mustacheExpress = require('mustache-express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var session = require('express-session');
+var parseurl = require('parseurl');
+
+var app = express();
+
+//database of users
+var users = [
+  {'username': 'Christina',
+   'password': 'codingrocks'},
+  {'username': 'Sami',
+   'password': '12345'},
+  {'username': 'Sera',
+   'password': '67890'}
+];
+
+//view engine
+app.engine('mustache', mustacheExpress());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'mustache');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extend: false
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//session middleware
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}));
+//check to see if we have a user and if pathname is not login
+app.use(function(req, res, next){
+  var pathname = parseurl(req).pathname;
+  if (!req.session.user && pathname != '/login'){
+    res.redirect('/login');
+  } else {
+
+    next();
+  }
+});
+
+//checking for views object, if there isn't one assigns value to 0
+app.use(function(req, res, next){
+  var views = req.session.views;
+  if (!views) {
+    views = req.session.views = {};
+  }
+  var pathname = parseurl(req).pathname;
+  views[pathname] = (views[pathname] || 0) +1;
+
+  next();
+});
+
+
+
+app.get('/login', function(req, res){
+  var context = {
+    next: req.query.next
+  };
+  res.render('login', context);
+});
+
+app.post('/login', function(req, res){
+  var username = req.body.username;
+  var password = req.body.password;
+
+//check username against one input on form
+var person = users.find(function(user){
+  return user.username === username;
+});
+
+
+if (person && person.password === password) {
+  req.session.user = person;
+} else if (req.session.user) {
+  delete req.session.user;
+}
+
+if (req.session.user) {
+  res.redirect('/');
+} else {
+  res.redirect('/login');
+}
+});
+
+app.get('/', function(req,res){
+res.send("Welcome " + req.session.user.username + " !");
+});
+
+app.listen(3000, function(){
+  console.log("app started successfully!");
+});
